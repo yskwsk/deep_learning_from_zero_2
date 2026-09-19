ecc314827666e8cf6e6c35b6e0b8602f23517a4c
yoshihiro-kawasaki <kawayoshi.0117@gmail.com>
2026-09-19 22:29:27 +0900

    common/util.pyの更新

--- src/common/util.py
+++ src/common/util.py
@@ -3,6 +3,127 @@
 from common.np import np, NDArray
 
 
+def preprocess(text: str) -> tuple[np.ndarray, dict[str, int], dict[int, str]]:
+    text = text.lower()
+    text = text.replace(".", " .")
+    words = text.split(" ")
+
+    word_to_id: dict[str, int] = {}
+    id_to_word: dict[int, str] = {}
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+
+    corpus = np.array([word_to_id[w] for w in words])
+
+    return corpus, word_to_id, id_to_word
+
+
+def most_similar(
+    query: str,
+    word_to_id: dict[str, int],
+    id_to_word: dict[int, str],
+    word_matrix: np.ndarray,
+    top: int = 5
+) -> None:
+    # クエリを取り出す
+    if query not in word_to_id:
+        print(f"{query} is not found")
+        return
+
+    print(f"\n[query] {query}")
+    query_id: int = word_to_id[query]
+    query_vec: np.ndarray = word_matrix[query_id]
+
+    # コサイン類似度の算出
+    vocab_size: int = len(id_to_word)
+    similarity: np.ndarray = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+
+    # コサイン類似度の結果から、その値を高い順に出力
+    count: int = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print(f" {id_to_word[i]}: {similarity[i]}")
+        count += 1
+        if count >= top:
+            return
+
+
+def cos_similarity(x: np.ndarray, y: np.ndarray, eps: float = 1e-8) -> float:
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
+
+
+def create_co_occurence_matrix(
+    corpus: np.ndarray,
+    vocab_size: int,
+    window_size: int = 1
+) -> np.ndarray:
+    """共起行列の作成"""
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
+
+
+def positive_pointwise_mutual_information(
+    co_occuerence_matrix: np.ndarray,
+    verbose: bool = False,
+    eps: float = 1.0e-8
+) -> np.ndarray:
+    '''PPMI（正の相互情報量）の計算'''
+    ppmi = np.zeros_like(co_occuerence_matrix, dtype=np.float32)
+    N = np.sum(co_occuerence_matrix)
+    S = np.sum(co_occuerence_matrix, axis=0)
+    total = co_occuerence_matrix.shape[0] * co_occuerence_matrix.shape[1]
+    cnt = 0
+
+    for i in range(co_occuerence_matrix.shape[0]):
+        for j in range(co_occuerence_matrix.shape[1]):
+            pmi = np.log2(co_occuerence_matrix[i, j] * N / (S[i] * S[j]) + eps)
+            ppmi[i, j] = max(0.0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total // 100) == 0:
+                    print(f"{100*cnt/total}% done")
+
+    return ppmi
+
+
+def positive_pointwise_mutual_information_fast(
+    co_occuerence_matrix: np.ndarray,
+    verbose: bool = False,
+    eps: float = 1.0e-8
+) -> np.ndarray:
+    '''PPMI（正の相互情報量）の計算の高速版'''
+    N = np.sum(co_occuerence_matrix, dtype=np.float64)
+    S = np.sum(co_occuerence_matrix, axis=0)
+    Sij = np.outer(S, S)
+    ppmi = np.log2(co_occuerence_matrix * N / (Sij) + eps).astype(np.float32)
+    ppmi = np.where(ppmi > 0.0, ppmi, 0.0)
+    return ppmi
+
+
 # def to_cpu(x):
 #     import numpy
 #     if isinstance(x, numpy.ndarray):
