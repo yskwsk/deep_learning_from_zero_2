0a0d0b3d557fc1d6bbc49c7823a27d5909ac06b1
yoshihiro-kawasaki <kawayoshi.0117@gmail.com>
2026-09-21 20:38:31 +0900

    ch04の更新

---
+++ src/ch04/cbow.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+from negative_sampling_layer import NegativeSamplingLoss
+from common.base_model import BaseModel
+from common.layers import Embedding
+from common.np import np, NDArray
+
+
+class CBOW(BaseModel):
+    def __init__(
+        self,
+        vocab_size: int,
+        hidden_size: int,
+        window_size: int,
+        corpus: np.ndarray
+    ) -> None:
+        # 重みの初期化
+        W_in = 1.0e-2 * np.random.randn(vocab_size, hidden_size).astype(np.float32)
+        W_out = 1.0e-2 * np.random.randn(vocab_size, hidden_size).astype(np.float32)
+
+        # レイヤの生成
+        self.layers = []
+        for i in range(2 * window_size):
+            self.layers.append(Embedding(W_in))
+        self.ns_loss = NegativeSamplingLoss(
+            W=W_out,
+            corpus=corpus,
+            power=0.75,
+            sample_size=5
+        )
+
+        # 全ての重みと勾配をリストにまとめる
+        self.params = []
+        self.grads = []
+        layers = self.layers + [self.ns_loss]
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # メンバ変数に単語の分散表現を設定
+        self.word_vecs = W_in
+
+    def forward(self, x: NDArray, t: NDArray) -> NDArray:
+        h = self.layers[0].forward(x[:, 0])
+        for i in range(1, len(self.layers)):
+            h += self.layers[i].forward(x[:, i])
+        h *= 1 / len(self.layers)
+        loss = self.ns_loss.forward(h, t)
+        return loss
+
+    def backward(self, dout: NDArray = np.array(1)) -> None:
+        dout = self.ns_loss.backward(dout)
+        dout *= 1 / len(self.layers)
+        for layer in self.layers:
+            layer.backward(dout)
+        return None
---
+++ src/ch04/train.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+
+import pickle
+from typing import Any
+
+from cbow import CBOW
+from common.np import np
+from common.optimizer import Adam
+from common.util import create_contexts_target
+from common.trainer import Trainer
+from dataset import ptb
+
+
+# ハイパーパラメータの設定
+window_size = 5
+hidden_size = 100
+batch_size = 100
+max_epoch = 100
+
+# データの読み込み
+corpus, word_to_id, id_to_word = ptb.load_data("train")
+vocab_size = len(word_to_id)
+contexts, target = create_contexts_target(
+    corpus=corpus,
+    window_size=window_size
+)
+
+# モデルの生成
+model = CBOW(
+    vocab_size=vocab_size,
+    hidden_size=hidden_size,
+    window_size=window_size,
+    corpus=corpus
+)
+optimizer = Adam()
+trainer = Trainer(
+    model=model,
+    optimizer=optimizer
+)
+
+# 学習開始
+trainer.fit(
+    x=contexts,
+    t=target,
+    max_epoch=max_epoch,
+    batch_size=batch_size,
+)
+trainer.plot()
+
+# 後ほど利用できるように必要なデータを保存
+word_vecs = model.word_vecs
+params: dict[str, Any] = {}
+params['word_vecs'] = word_vecs.astype(np.float16)
+params['word_to_id'] = word_to_id
+params['id_to_word'] = id_to_word
+pkl_file = "cbow_params.pkl"
+# pkl_file = "skipgram_params.pkl"
+with open(pkl_file, 'wb') as file:
+    pickle.dump(params, file, -1)
