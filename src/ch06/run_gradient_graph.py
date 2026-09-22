1bc570ba0bd72d46fa1239cf54cab6527c1f3579
yoshihiro-kawasaki <kawayoshi.0117@gmail.com>
2026-09-23 00:04:11 +0900

    ch06の追加

---
+++ src/ch06/run_gradient_graph.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+N = 2  # ミニバッチサイズ
+H = 3  # 隠れ状態ベクトルの次元数
+T = 20  # 時系列データの長さ
+
+dh = np.ones((N, H))
+
+np.random.seed(3)
+
+# 勾配爆発の場合
+# Wh = np.random.randn(H, H)
+# 勾配消失の場合
+Wh = np.random.randn(H, H) * 0.5
+
+norm_list = []
+for t in range(T):
+    dh = np.dot(dh, Wh.T)
+    norm = np.sqrt(np.sum(dh**2)) / N
+    norm_list.append(norm)
+
+print(norm_list)
+
+# グラフの描画
+plt.plot(np.arange(len(norm_list)), norm_list)
+plt.xticks([0, 4, 9, 14, 19], [str(i) for i in [1, 5, 10, 15, 20]])
+plt.xlabel('time step')
+plt.ylabel('norm')
+plt.show()
